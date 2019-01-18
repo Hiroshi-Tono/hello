1d1ce4db38b2dd7a6794361323b9e6866cbf0cd6
Hiroshi Tone <valkyrie.gtr@gmail.com>
2019-01-19 00:35:49 +0900

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by 遠野ひろし on 2019/01/19.
+//  Copyright © 2019 遠野工房. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
