dc2e8fa8add2cde5d2c6b7c8d9f62ba644fd66da
Hiroshi Tone <valkyrie.gtr@gmail.com>
2019-01-19 02:06:18 +0900

    ローカライズの実習が完了。

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myLabel.text = "test"
+        myLabel.text = NSLocalizedString("test", comment: "")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
